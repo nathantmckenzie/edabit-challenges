1a046fcce4e83dfea8bcbafecd0931f412a64d66
Nathan McKenzie <nathantaylormckenzie@hotmail.com>
2021-05-07 13:46:07 -0700

    longest increasing subsequence

--- lis.js
+++ lis.js
@@ -2,27 +2,60 @@
 //medium
 //unsolved
 
-var lengthOfLIS = function (nums) {
-  let arr = [];
-  let k = 1;
-  let i = 0;
-  let increasing = 0;
-  while (i < nums.length) {
-    if (nums[k + 1] > nums[k]) {
-      k++;
-      increasing++;
-    } else {
-      k++;
-    }
-    if (k === nums.length) {
-      arr.push(increasing);
-      console.log(arr);
-      i++;
-      k = i + 1;
-      increasing = 0;
+// var lengthOfLIS = function (sequence) {
+//   const lengthsArray = Array(sequence.length).fill(1);
+
+//   let previousElementIndex = 0;
+//   let currentElementIndex = 1;
+
+//   while (currentElementIndex < sequence.length) {
+//     if (sequence[previousElementIndex] < sequence[currentElementIndex]) {
+//       // If current element is bigger then the previous one then
+//       // current element is a part of increasing subsequence which
+//       // length is by one bigger then the length of increasing subsequence
+//       // for previous element.
+//       const newLength = lengthsArray[previousElementIndex] + 1;
+//       if (newLength > lengthsArray[currentElementIndex]) {
+//         // Increase only if previous element would give us bigger subsequence length
+//         // then we already have for current element.
+//         lengthsArray[currentElementIndex] = newLength;
+//       }
+//     }
+
+//     // Move previous element index right.
+//     previousElementIndex += 1;
+
+//     // If previous element index equals to current element index then
+//     // shift current element right and reset previous element index to zero.
+//     if (previousElementIndex === currentElementIndex) {
+//       currentElementIndex += 1;
+//       previousElementIndex = 0;
+//     }
+//   }
+//   // Find the biggest element in lengthsArray.
+//   // This number is the biggest length of increasing subsequence.
+//   let longestIncreasingLength = 0;
+
+//   for (let i = 0; i < lengthsArray.length; i += 1) {
+//     if (lengthsArray[i] > longestIncreasingLength) {
+//       longestIncreasingLength = lengthsArray[i];
+//     }
+//   }
+
+//   return longestIncreasingLength;
+// };
+// console.log(lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]));
+
+function lis(arr) {
+  let lengthsArray = Array(arr.length).fill(1);
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] < arr[i]) {
+        lengthsArray[i] = Math.max(lengthsArray[i], lengthsArray[j] + 1);
+      }
     }
   }
-  return arr;
-};
+  return Math.max(...lengthsArray);
+}
 
-console.log(lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]));
+console.log(lis([10, 9, 2, 5, 3, 7, 101, 18]));
---
+++ symmetric-difference.js
@@ -0,0 +1,9 @@
+//https://www.freecodecamp.org/learn/coding-interview-prep/algorithms/find-the-symmetric-difference
+
+function sym(args) {
+  let arr = [];
+  for ()
+  return args;
+}
+
+console.log(sym([1, 2, 3], [5, 2, 1, 4]));
