442b666dea2f2598b1a1a3757403eeca6c1b57dc
Nathan <nathan.mckenzie@improving.com>
2021-11-01 17:11:04 -0700

    .

--- connecting-words.js
+++ connecting-words.js
@@ -2,16 +2,58 @@
 //expert
 //solved
 
+// const join = (arr) => {
+//   let fullString = "";
+//   let i = 0;
+//   let k = 0;
+//   let overLapping = arr[i];
+//   let numbers = [];
+//   while (i < arr.length - 1) {
+//     if (arr[i + 1].startsWith(overLapping)) {
+//       numbers.push(arr[i].length - k);
+
+//       if (i === 0) {
+//         fullString += arr[i].substring(0, k) + arr[i + 1];
+//       } else {
+//         let length = arr[i].length - k;
+//         fullString += arr[i + 1].substring(length);
+//       }
+//       i++;
+//       k = 0;
+//     } else {
+//       k++;
+//       overLapping = arr[i].substring(k);
+//     }
+//   }
+//   let minimumValue = Math.min(...numbers);
+//   if (minimumValue > 0) {
+//     return [fullString, minimumValue];
+//   } else {
+//     return [
+//       fullString.concat(arr[arr.length - 2].concat(arr[arr.length - 1])),
+//       0,
+//     ];
+//   }
+// };
+
+// const join = (A, c = []) => {
+//   return [
+//     A.reduce(function (s, n) {
+//       console.log("s", s, "n", n);
+//       console.log("c", c);
+//       return `${s},${n}`.replace(/(.*),\1/g, (_, p) => c.push(p.length) && p);
+//     }),
+//     Math.min(...c),
+//   ];
+// };
+
 const join = (arr) => {
-  let fullString = "";
   let i = 0;
   let k = 0;
+  let fullString = "";
   let overLapping = arr[i];
-  let numbers = [];
   while (i < arr.length - 1) {
     if (arr[i + 1].startsWith(overLapping)) {
-      numbers.push(arr[i].length - k);
-
       if (i === 0) {
         fullString += arr[i].substring(0, k) + arr[i + 1];
       } else {
@@ -25,15 +67,7 @@ const join = (arr) => {
       overLapping = arr[i].substring(k);
     }
   }
-  let minimumValue = Math.min(...numbers);
-  if (minimumValue > 0) {
-    return [fullString, minimumValue];
-  } else {
-    return [
-      fullString.concat(arr[arr.length - 2].concat(arr[arr.length - 1])),
-      0,
-    ];
-  }
+  return fullString;
 };
 
 console.log(join(["oven", "envier", "erase", "serious"]));
--- photo-letter-combos.js
+++ photo-letter-combos.js
@@ -1,6 +1,6 @@
 //edabit.com/challenge/npLurjMJofmFRCJwx
-https: expert;
-unsolved;
+//expert
+//unsolved
 
 function letterCombinations(digits) {
   const obj = {
@@ -14,9 +14,34 @@ function letterCombinations(digits) {
     9: ["w", "x", "y", "z"],
   };
   let arr = [];
-  let combinations = digits
-    .split("")
-    .reduce((acc, cur) => acc * obj[cur].length);
+  let split = digits.split("").map(Number);
+  for (let i = 0; i < obj[split[0]].length; i++) {
+    for (let k = 0; k < obj[split[1]].length; k++) {
+      if (split.length > 2) {
+        for (let j = 0; j < obj[split[2]].length; j++) {
+          arr.push(obj[split[0]][i] + obj[split[1]][k] + obj[split[2]][j]);
+        }
+      } else {
+        arr.push(obj[split[0]][i] + obj[split[1]][k]);
+      }
+    }
+  }
+  return arr;
 }
 
-console.log(letterCombinations("23"));
+console.log(letterCombinations("543"));
+
+// const letterCombinations = (str) => {
+//   const arr = [
+//     ["a", "b", "c"],
+//     ["d", "e", "f"],
+//   ];
+//   const split = str.split("");
+//   let storage = [];
+//   for (let i = 0; i < arr[0].length; i++) {
+//     for (let k = 0; k < arr[1].length; k++) {
+//       storage.push(arr[0][i] + arr[1][k]);
+//     }
+//   }
+//   return storage;
+// };
